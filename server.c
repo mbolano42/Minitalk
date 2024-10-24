d78a40b88a0c280467c055c291a0448fec24b460
Miguel Bolaño Ruiz <mbolano-@student.42malaga.com>
2024-10-24 15:22:33 +0200

    Se me olvidó añadir un printf para mostrar el PID del programa server.c

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mbolano- <mbolano-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:16:13 by mbolano-          #+#    #+#             */
-/*   Updated: 2024/10/24 15:19:29 by mbolano-         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:21:01 by mbolano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@ void	ft_signal_handler(void)
 
 int main(void)
 {
+	long int	pid;
+
+	pid = getpid();
+	printf("El PID del programa servidor es: %ld", pid);
 	while (1)
 	{
 		signal(SIGUSR1, &ft_signal_handler);
