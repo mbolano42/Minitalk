c014dd47adc88a01f9db4111670c4455a6831cf8
Miguel Bolaño Ruiz <mbolano-@student.42malaga.com>
2024-10-24 15:07:51 +0200

    Creadas las funciones ft_chksign y ft_atoi

---
+++ client.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbolano- <mbolano-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 13:50:34 by mbolano-          #+#    #+#             */
+/*   Updated: 2024/10/24 15:06:02 by mbolano-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+long int	ft_chksign(char c)
+{
+	if (c == '-')
+		return (-1);
+	return (1);
+}
+
+long int	ft_atoi(char *src)
+{
+	long int 	sign;
+	long int	result;
+	
+
+	sign = 1;
+	result = 0;
+	while (*src == ' ' || *src == '\t' || *src == '\n' || *src == '\v' || *src == '\f' || *src == '\r')
+		src++;
+	if (*src == '+' || *src == '-')
+		sign *= ft_chksign(*src++);
+	while (*src)
+	{
+		if (*src >= '0' && *src <= '9')
+			result = result * 10 + ((*src ) - '0');
+		else
+			return (0); 
+		src++;
+	}
+	return (sign * result);
+}
+
+void	ft_input_parser(long int pid, char *str)
+{
+	if (pid <= 0)
+		exit(EXIT_FAILURE);
+	printf("El valor del pid es: %ld", pid);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+		ft_input_parser(ft_atoi(argv[1]), argv[2]);
+	return (0);
+}
