d254afe402455466b7b542e6a4a13b9ce8b1644d
Miguel Bolaño Ruiz <mbolano-@student.42malaga.com>
2024-10-24 23:06:31 +0200

    Añado los últimos algoritmos implementados en server.c y client.c

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mbolano- <mbolano-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:50:55 by mbolano-          #+#    #+#             */
-/*   Updated: 2024/10/24 16:41:54 by mbolano-         ###   ########.fr       */
+/*   Updated: 2024/10/24 23:05:47 by mbolano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@
 # include <stdlib.h>
 # include <stdio.h>
 
+// Algoritmos implementados en el cliente:
 long int	ft_chksign(char c);
 long int	ft_atoi(char *src);
+void		ft_send_byte_to_server(char *byte, int pid);
+void		ft_char_to_binary(int letter, char *byte);
 void		ft_input_parser(long int pid, char *str);
+// Algoritmos implementados en el servidor:
 void		ft_signal_handler(int signum);
 void		ft_fill_nbr(char *nbr);
 void		ft_putnbr(int pid);
