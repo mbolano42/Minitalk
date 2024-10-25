c74aaccb5ce6845643089d92fbfc15b2c84c6566
Miguel Bolano Ruiz <mbolano-@c3r10s5.42malaga.com>
2024-10-25 11:45:15 +0200

    Modificado/simplificado client.c a sólo 3 funciones

--- client.c
+++ client.c
@@ -6,63 +6,53 @@
 /*   By: mbolano- <mbolano-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:50:34 by mbolano-          #+#    #+#             */
-/*   Updated: 2024/10/24 22:54:48 by mbolano-         ###   ########.fr       */
+/*   Updated: 2024/10/25 11:07:43 by mbolano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-long int	ft_chksign(char c)
-{
-	if (c == '-')
-		return (-1);
-	return (1);
-}
-
 long int	ft_atoi(char *src)
 {
-	long int 	sign;
+	long int	sign;
 	long int	result;
-	
 
 	sign = 1;
 	result = 0;
-	while (*src == ' ' || *src == '\t' || *src == '\n' || *src == '\v' || *src == '\f' || *src == '\r')
+	while (*src == ' ' || *src == '\t' || *src == '\n' || *src == '\v'
+		|| *src == '\f' || *src == '\r')
 		src++;
 	if (*src == '+' || *src == '-')
-		sign *= ft_chksign(*src++);
+	{
+		if (*src == '-')
+			sign *= -1;
+		src++;
+	}
 	while (*src)
 	{
 		if (*src >= '0' && *src <= '9')
-			result = result * 10 + ((*src ) - '0');
+			result = result * 10 + ((*src) - '0');
 		else
-			return (0); 
+			return (0);
 		src++;
 	}
 	return (sign * result);
 }
 
-void	ft_send_byte_to_server(char *byte, int pid)
-{
-	while (*byte)
-	{
-		if (*byte == '0')
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		byte++;
-		usleep(150);
-	}
-}
-
-void	ft_char_to_binary(int letter, char *byte)
+void	ft_send_byte_to_server(int letter, long int pid)
 {
 	int	i;
+	int	bit;
 
 	i = 0;
 	while (i < 8)
 	{
-		byte[i] = letter % 2 + '0';
+		bit = letter % 2;
+		if (bit == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
 		letter /= 2;
 		i++;
 	}
@@ -70,28 +60,13 @@ void	ft_char_to_binary(int letter, char *byte)
 
 void	ft_input_parser(long int pid, char *str)
 {
-	char	*byte;
-	int		i;
-
-	byte = (char *)malloc(sizeof(char) + 9);
-	if (!byte || pid <= 0 || kill(pid, SIGUSR1) != 0)
-	{
-		free(byte);
+	if (pid <= 0 || kill(pid, SIGUSR1) != 0)
 		exit(EXIT_FAILURE);
-	}
 	while (*str)
 	{
-		ft_char_to_binary((int)*str, byte);
-		ft_send_byte_to_server(byte, (int)pid);
-		i = 0;
-		while (i < 9)
-		{
-			byte[i] = '\0';
-			i++;
-		}
+		ft_send_byte_to_server((int)*str, pid);
 		str++;
 	}
-	free(byte);
 }
 
 int	main(int argc, char **argv)
