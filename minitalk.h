f46e45cec1045b9c5463cc6b15cbd9bd818aac42
Miguel Bolano Ruiz <mbolano-@c3r10s5.42malaga.com>
2024-10-25 14:11:38 +0200

    modificada la función ft_send_byte_to_server para que interprete caracteres UNICODE

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mbolano- <mbolano-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:50:55 by mbolano-          #+#    #+#             */
-/*   Updated: 2024/10/25 10:59:19 by mbolano-         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:06:40 by mbolano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 // Algoritmos implementados en el cliente:
 long int	ft_atoi(char *src);
-void		ft_send_byte_to_server(int letter, long int pid);
+void		ft_send_byte_to_server(unsigned char letter, long int pid);
 void		ft_input_parser(long int pid, char *str);
 // Algoritmos implementados en el servidor:
 void		ft_signal_handler(int signum);
